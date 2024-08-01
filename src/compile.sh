f048e937e734ac3c10f36ba32d7765c1cf696b24
Andrea Ciccarello <58073848+AndreaCicca@users.noreply.github.com>
2024-08-01 14:20:35 +0200

    update Cmake list

--- src/compile.sh
+++ src/compile.sh
@@ -4,3 +4,6 @@ conan install . --output-folder=build --build=missing
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
+
+
+# cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
